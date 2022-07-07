bf52059f3c1ec4ac8c0b6d62afd62e9d88c39fd6
otorres828 <olivertorres1997@gmail.com>
2022-07-08 01:15:43 +0800

    livewire

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,6 +9,7 @@
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" rel="stylesheet">    
     @yield('css')
     <title>@yield('title')</title>
+    @livewireStyles
 
 </head>
 <body >
