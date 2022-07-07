7353b548c7a1e267b966acd9f48ad17cece03da8
otorres828 <olivertorres1997@gmail.com>
2022-07-07 23:19:54 +0800

    ad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,22 +5,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <title>{{ config('app.name', 'Mansion de Cristo') }}</title>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Styles -->
-        @livewireStyles
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @livewireStyles
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
+        
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            @include('components.aminblog.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -38,9 +37,6 @@
         </div>
 
         @stack('modals')
-        @stack('js')
-        @stack('css')
-        <script src="{{ mix('js/app.js') }}"></script>
 
         @livewireScripts
     </body>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,22 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" rel="stylesheet">   
-    @yield('css')
-    @yield('head')
-    @livewireStyles
-    <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" rel="stylesheet">    
+    @yield('css')
+    <title>@yield('title')</title>
 
 </head>
 <body >
-    @livewire('navigation-menu')
 
     @yield('main')
-           <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <script src="{{ mix('js/app.js') }}"></script>
+    @yield('js')
     @livewireScripts
-   </body>
+</body>
 </html>
